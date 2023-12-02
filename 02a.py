0a00822865dd1e92bfaac1ab5f978cd4cbd29fd3
prls59 <119790965+prls59@users.noreply.github.com>
2023-12-02 13:14:17 +0000

    aoc 2a

---
+++ 02a.py
@@ -0,0 +1,25 @@
+datafile = "../../AoC/2023/02.txt"
+limits = {'red': 12, 'green': 13, 'blue': 14}
+
+try:
+    input = open(datafile)
+    idsum = 0
+    for line in input:
+        line = line[0:-1] # strip off \n
+        game = line.split(": ")
+        id = int(game[0][5:])
+        rounds = game[1].split("; ")
+        for round in rounds:
+            colours = round.split(", ")
+            for colour in colours:
+                cubes = colour.split(" ")
+                if int(cubes[0])>limits[cubes[1]]:
+                    id = 0
+                    break
+            if id == 0:
+                break
+        idsum += id
+    print('Result = ', idsum)
+
+finally:
+    input.close()
